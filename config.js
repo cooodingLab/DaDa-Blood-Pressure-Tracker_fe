a1c4366957de2c29ae4baf60094393743262733f
monster <joycechen.event@gmail.com>
2025-12-09 19:30:30 +0800

    feat: 加入 API Secret 驗證

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
 // 請務必確認這裡填的是您 Apps Script 部署後的正確網址
-const API_URL = "https://script.google.com/macros/s/AKfycbxDHXqA1aU729NtAewvASLgg4vZOGdUdsi2ZbjTS4QwwfxrBqS18Ptkq28IgkCaJjOx/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwhtlNdhdsR52RbCDXy5oAVjZpeU-Pq9VNy3F_lO_SaQ5eYjstafKhgtGr-2Mxim8vV/exec";
